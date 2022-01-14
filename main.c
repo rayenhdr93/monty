56c753eee2298d1604ee0564b73299d86c231175
rayenhdr93 <hedrirayen69@gmail.com>
2022-01-14 10:29:02 +0100

    ff

--- main.c
+++ main.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 2) 
 		{
-			fprintf(stderr, "USAGE: monty file");
+			fprintf(stderr, "USAGE: monty file\n");
 			exit(EXIT_FAILURE);
 		}
 	fp = fopen(argv[1], "r");
 	if (!fp)
 		{
-			fprintf(stderr, "Error: Can't open file %s", argv[1]);
+			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 			exit(EXIT_FAILURE);
 		}
 	while (1)
