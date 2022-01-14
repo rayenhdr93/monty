4f737c21f67b6d0f622fab52890c67ecabd4ab0a
rayenhdr93 <hedrirayen69@gmail.com>
2022-01-14 18:22:26 +0100

    \n

--- pop.c
+++ pop.c
@@ -10,7 +10,10 @@ void pop(stack_t **stack, unsigned int line_number)
 	stack_t *tmp = NULL;
 
 	if (!(*stack))
+	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	if (*stack)
 	{
 		tmp = (*stack)->next;
